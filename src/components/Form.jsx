49bb93903e852be8debe187646e45babba74ab72
manodecristiano <cristian.carre.online@gmail.com>
2022-05-21 12:10:50 +0200

    All configurate READY

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+
+
+function Form() {
+  return (
+    <div className="bg-red-700 text-cyan-500">
+        
+        <h1 className="bg-red-700 text-cyan-500">Form</h1>
+
+    </div>
+  )
+}
+
+export default Form
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode:'jit',
+  purge:["./index.html","./src/**/*.{html,js,jsx}"],
+  content:  ["./src/**/*.{html,js,jsx}"],
+  darkMode:false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
