f73292dd190b7e7f9bed5cadab8ba6ffca286166
manodecristiano <cristian.carre.online@gmail.com>
2022-05-25 00:00:29 +0200

    Form with Input

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { PacientList } from './components/PacientList.jsx';
 
 function App() {
   return (
-    <div className='container mx-auto mt-20 '>
+    <div className='container mx-auto mt-20 bg-slate-300 h-screen'>
       <Header/>
     <div className='mt-12 md:flex'>
       <Form/>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,14 +4,28 @@ function Form() {
   return (
   <div className="md:w-1/2 lg:w-2/5">
   
-  <h2 className=" font-black text-3xl text-center">Seguimiento Pacientes</h2>
-    <p className="text-lg mt-5 text-center">
-    Añade Pacientes y{' '}
-    <span className="text-indigo-600 font-bold text-lg">Administralos</span>
+    <h2 className=" font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y{' '}
+      <span className="text-indigo-600 font-bold text-lg">Administralos</span>
     </p>
-  
+
+    <form className="bg-white shadow-md rounded-lg py-10 px-5">
+      <div>
+        <label htmlFor="mascota" className="block text-gray-700 uppercase ">Nombre Mascota</label>
+
+        <input 
+        id="mascota"
+        type="text"
+        placeholder="Nombre Mascota"
+        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+        />
+
+
+      </div>
+    </form>
   </div>
-  
+
   )
 }
 
